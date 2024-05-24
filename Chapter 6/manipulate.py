2cb8c31c922d1dbcda18df77c5e6db232edb5f0b
John Bower <bowerjohnd@gmail.com>
2024-05-23 23:31:32 -0400

    Chapter 6

---
+++ Chapter 6/manipulate.py
@@ -0,0 +1,16 @@
+def display(s) :
+	'''Display an argument here.'''
+	print(s)
+
+# _ _ doc _ _ needs double underscores on each side
+
+display(display.__doc__)		
+
+display(r"C:\Program Files")
+
+display("\nHello" + " Python")
+
+display("Python in Easy Steps\n" [7 :])
+
+display("P" in "Python")
+display("p" in "Python")
